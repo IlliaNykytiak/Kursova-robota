89f3e9343a2d5bd59b84d794e768c384c89f9243
Acer <Acer@DESKTOP-72VISB5>
2024-05-01 15:01:26 +0300

    git ignore

--- WebApplication1/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1
-{
-    public class Constants
-    {
-        public static string Address = "https://ott-details.p.rapidapi.com";
-        public static string ApiKey = "8ef1f02da7msh7743f43f596e4a3p1dd3e3jsn11e6fc5f2a81";
-        public static string ApiHost = "ott-details.p.rapidapi.com";
-    }
-}
