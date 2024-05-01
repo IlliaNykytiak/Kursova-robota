33559f952d7c7b9cd8db80efbc79a177eeabf163
Acer <Acer@DESKTOP-72VISB5>
2024-05-01 13:02:30 +0300

    Add project files.

---
+++ WebApplication1/Clients/MovieClient.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Clients
+{
+    public class MovieClient
+    {
+        private static string _address;
+        private static string _apikey;
+        private static string _apihost;
+        public MovieClient()
+        {  
+            _address = Constants.Address;
+            _apikey = Constants.ApiKey;
+            _apihost = Constants.ApiHost;
+
+        }
+        public async Task<MovieList> GetMovieListIMDBRating(int start_year, int end_year, double min_imdb, double max_imdb)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(_address + "/advancedsearch?start_year=" + start_year + "&end_year=" + end_year + "&min_imdb=" + min_imdb +"&max_imdb=" + max_imdb + "&genre=adventure&language=english&type=movie&sort=oldest&page=1"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", _apikey },
+                    { "X-RapidAPI-Host", _apihost },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<MovieList>(body);
+                return result;
+            }
+
+        }
+    }
+}
---
+++ WebApplication1/Controllers/MovieListController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Clients;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class MovieListController : ControllerBase
+    {
+        private readonly ILogger<MovieListController> _logger;
+        public MovieListController (ILogger<MovieListController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public List<string> MovieTitles(int start_year, int end_year, double min_imdb, double max_imdb)
+        {
+            MovieClient client = new MovieClient();
+            MovieList movieList = client.GetMovieListIMDBRating(start_year, end_year, min_imdb, max_imdb).Result;
+            List<string> titles = new List<string>();
+            List<double> rating = new List<double>();
+
+            if (movieList.results != null)
+            {
+                foreach (var result in movieList.results)
+                {
+                    titles.Add(result.title);
+                }
+                //foreach (var result in movieList.results)
+                //{
+                //    rating.Add(result.imdbrating);
+                //}
+            }
+            return titles;
+        }
+    }
+}
